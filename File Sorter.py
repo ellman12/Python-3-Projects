dd15b6ea6ffcc2c1e7920a3083a027d725ab1a09
Elliott DuCharme <ellduc4@gmail.com>
2020-12-10 14:00:56 -0600

    WORKS SOMEHOW

--- File Sorter.py
+++ File Sorter.py
@@ -7,6 +7,7 @@
 # Ideas: have a way to rename the current file in the loop if the
 # user wants???
 # Different modes for sorting. Option to put them by day too.
+# Move files with errors to unsorted folder.
 # "also, i would reccomment zero padding the month folder, so it's 01 for january, that way it sorts well"
 #######################################################################
 
@@ -41,15 +42,15 @@ for dirPath, _, files in os.walk(dirToSort):
             if (EXIF_DATETIME_TAG in exif_data):
                 fileTakenDate = datetime.datetime.strptime(exif_data[EXIF_DATETIME_TAG], "%Y:%m:%d %H:%M:%S")
 
-                # filePath = os.path.join(dirPath, file)
-                filePath = dirPath + r"/" + file
+                filePath = os.path.join(dirPath, file)
+                # filePath = dirPath + r"/" + file
 
-                # destinationPath = os.path.join(destinationDir, str(fileTakenDate.year), str(fileTakenDate.month))
-                destinationPath = destinationDir + r"/" + str(fileTakenDate.year) + r"/" + str(fileTakenDate.month)
+                destinationPath = os.path.join(destinationDir, str(fileTakenDate.year), str(fileTakenDate.month))
+                # destinationPath = destinationDir + r"/" + str(fileTakenDate.year) + r"/" + str(fileTakenDate.month)
 
                 if not os.path.exists(destinationPath):
                     os.makedirs(destinationPath)
-                shutil.copyfile(filePath, destinationPath)
+                shutil.copyfile(filePath, os.path.join(destinationPath, file))
 
                 print(f"{file} at {filePath} moved to {destinationPath}")
 
